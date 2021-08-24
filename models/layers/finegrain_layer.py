d078c9be4c6d1f11c7500332c0223aeb4245dbba
Hoang Le <hoangle1001@outlook.com>
2021-08-24 17:24:07 +0700

    Add function discarding special tokens after embedding

--- models/layers/finegrain_layer.py
+++ models/layers/finegrain_layer.py
@@ -38,6 +38,17 @@ class FineGrain(torch_nn.Module):
             torch_nn.Linear(d_bert, d_hid // 2),
         )
 
+    def remove_special_toks(self, a, a_masks):
+        bz = a.size(0)
+
+        a_ = torch.zeros_like(a)
+
+        for i in range(bz):
+            l = a_masks[i].sum() - 2
+            a_[i, :l] = a[i, 1 : 1 + l]
+
+        return a_
+
     def encode_ques_para(self, q_ids, c_ids, q_masks, c_masks):
         # q_ids: [b, l_q]
         # c_ids: [b, n_c, l_c]
@@ -47,7 +58,9 @@ class FineGrain(torch_nn.Module):
         n_c = c_ids.size(1)
 
         q = self.bert_emb(input_ids=q_ids, attention_mask=q_masks)[0]
-        # q  : [b, l_q, d_bert]
+        # [b, l_q, d_bert]
+        q = self.remove_special_toks(q, q_masks)
+        # [b, l_q, d_bert]
 
         #########################
         # Operate CoAttention question
@@ -63,7 +76,9 @@ class FineGrain(torch_nn.Module):
             # Embed c
             ###################
             L_s = self.bert_emb(input_ids=contx, attention_mask=contx_mask)[0]
-            # L_s: [b, l_c, d_bert]
+            # [b, l_c, d_bert]
+            L_s = self.remove_special_toks(L_s, contx_mask)
+            # [b, l_c, d_bert]
 
             ###################
             # Operate CoAttention between
