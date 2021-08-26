Fix some errors in model, metric calculation and config

models/chime.py
@@ -63,8 +63,8 @@ class CHIME(torch_nn.Module):
             device=q.device,
         )
         # NOTE: If not work, use the next of following instead of the following
-        qca_masks = torch.zeros_like(qca_ids)
-        # qca_masks = torch.ones((bz, qca_ids.size(1), qca_ids.size(1)), device=device)
+        # qca_masks = torch.zeros_like(qca_ids)
+        qca_masks = torch.ones((bz, qca_ids.size(1), qca_ids.size(1)), device=device)
         qca_tok_type_id = torch.ones_like(qca_ids)
         trgs = []
         lq_np, lc_np, la_np = [], [], []
@@ -98,8 +98,8 @@ class CHIME(torch_nn.Module):
             )
             l = 1 + lq_ + 1 + lc_ + 1
             # NOTE: If not work, use the next of following instead of the following
-            qca_masks[i, :l] = torch.ones((l,), dtype=torch.long, device=device)
-            # qca_masks[i] = self._get_padding_mask(qca_ids[i], l)
+            # qca_masks[i, :l] = torch.ones((l,), dtype=torch.long, device=device)
+            qca_masks[i] = self._get_padding_mask(qca_ids[i], l)
 
             qca_tok_type_id[i, : 1 + lq_ + 1] = torch.zeros(
                 (1 + lq_ + 1,), dtype=torch.long, device=device
@@ -113,7 +113,7 @@ class CHIME(torch_nn.Module):
                 [
                     a_np,
                     torch.full((1,), self.sep_id, dtype=torch.long, device=device),
-                    torch.full((self.la - la_,), self.pad_id, dtype=torch.long, device=device),
+                    torch.full((self.la - la_ + 1,), self.pad_id, dtype=torch.long, device=device),
                 ],
                 dim=-1,
             )
@@ -137,9 +137,9 @@ class CHIME(torch_nn.Module):
                 [output[1 : 1 + lq_], output[l1 : l1 + lc_], output[l2 : l2 + l_pad_q + l_pad_c]],
                 dim=0,
             )
-            l1 = 1 + lq_ + 1 + lc_ + 1
+            l1 = 1 + lq_ + 1 + lc_
             l2 = 1 + lq_ + 1 + lc_ + 1 + la_ + 1 + l_pad_q + l_pad_c
-            p2 = torch.cat([output[l1 : l1 + la_ + 1], output[l2:]], dim=0)
+            p2 = torch.cat([output[l1 : l1 + 1 + la_ + 1], output[l2:]], dim=0)
 
             part1.append(p1.unsqueeze(0))
             part2.append(p2.unsqueeze(0))
@@ -185,7 +185,7 @@ class CHIME(torch_nn.Module):
                 a_mem = a_gate * a_mem + (1 - a_gate) * part2
 
         output_mle = self.decoder(a_mem)
-        # [b, la + 1, d_vocab]
+        # [b, la + 2, d_vocab]
 
         return output_mle.transpose(1, 2), trgs
 
@@ -202,11 +202,13 @@ class CHIME(torch_nn.Module):
 
             for _ in range(la):
                 output, _ = self(q_, c_, a)
-                topi = torch.log_softmax(output, dim=1).argmax(dim=1)
+                topi = torch.log_softmax(output[:, :, a.size(-1)], dim=-1).argmax(dim=-1)
                 if topi == self.sep_id:
                     break
-                a = torch.cat((a, topi), dim=-1)
+                a = torch.cat((a, topi.unsqueeze(-1)), dim=-1)
 
-            ans.append(a)
+            a_ = torch.full((1, la), self.pad_id, dtype=a.dtype, device=a.device)
+            a_[0, : a.size(-1)] = a[0]
+            ans.append(a_)
 
         return torch.cat(ans, dim=0)

models/narrative_model.py
@@ -53,8 +53,6 @@ class NarrativeModel(plt.LightningModule):
     ####################################################################
 
     def get_prediction(self, output_mle, a1_ids, a2_ids):
-        prediction = torch.argmax(output_mle, dim=1)
-
         prediction = [
             {
                 "pred": " ".join(self.bert_tokenizer.convert_ids_to_tokens(pred_)),
@@ -63,7 +61,7 @@ class NarrativeModel(plt.LightningModule):
                     " ".join(self.bert_tokenizer.convert_ids_to_tokens(ans2_)),
                 ],
             }
-            for pred_, ans1_, ans2_ in zip(prediction.squeeze(1), a1_ids, a2_ids)
+            for pred_, ans1_, ans2_ in zip(output_mle, a1_ids, a2_ids)
         ]
 
         return prediction
@@ -75,7 +73,6 @@ class NarrativeModel(plt.LightningModule):
 
         loss = self.criterion(output_mle, trgs)
         self.log("train/loss", loss, on_step=True, on_epoch=True, prog_bar=False)
-        pred = torch.argmax(output_mle, dim=1)
 
         return {
             "loss": loss,
@@ -105,7 +102,7 @@ class NarrativeModel(plt.LightningModule):
         return None
 
     def validation_step(self, batch, batch_idx):
-        pred = self.model(batch["q_ids"], batch["c_ids"], batch["a1_ids"])[0]
+        pred = self.model.do_predict(batch["q_ids"], batch["c_ids"], 10)
 
         return {
             "pred": (
@@ -124,10 +121,10 @@ class NarrativeModel(plt.LightningModule):
             json.dump(preds, pred_file, indent=2, ensure_ascii=False)
 
         bleu_1, bleu_4, meteor, rouge_l = get_scores(preds)
-        self.log("train/bleu_1", bleu_1, on_epoch=True, prog_bar=False)
-        self.log("train/bleu_4", bleu_4, on_epoch=True, prog_bar=False)
-        self.log("train/meteor", meteor, on_epoch=True, prog_bar=False)
-        self.log("train/rouge_l", rouge_l, on_epoch=True, prog_bar=False)
+        self.log("valid/bleu_1", bleu_1, on_epoch=True, prog_bar=False)
+        self.log("valid/bleu_4", bleu_4, on_epoch=True, prog_bar=False)
+        self.log("valid/meteor", meteor, on_epoch=True, prog_bar=False)
+        self.log("valid/rouge_l", rouge_l, on_epoch=True, prog_bar=False)
 
     def configure_optimizers(self):
         no_decay = ["bias", "LayerNorm.weight"]

utils/model_utils.py
@@ -16,49 +16,42 @@ def get_scores(outputs, eps=10e-8):
     n_samples = 0
     bleu_1, bleu_4, meteor, rouge_l = 0, 0, 0, 0
     for pair in outputs:
-        try:
 
-            pred = process_sent(pair["pred"])
-            ref = list(map(process_sent, pair["ref"]))
+        pred = process_sent(pair["pred"])
+        ref = list(map(process_sent, pair["ref"]))
 
-            if pred == "":
-                return 0, 0, 0, 0
-
-            # Calculate BLEU score
-            ref_ = [x.split() for x in ref]
-            pred_ = pred.split()
-
-            bleu_1 = sentence_bleu(ref_, pred_, weights=(1, 0, 0, 0))
-            bleu_4 = sentence_bleu(ref_, pred_, weights=(0.25, 0.25, 0.25, 0.25))
-
-            # Calculate METEOR
-            meteor = meteor_score(ref, pred)
-
-            # Calculate ROUGE-L
-            scores = np.array(
-                [
-                    Rouge().get_scores(ref_, pred, avg=True)["rouge-l"]["f"] if ref != "" else 0
-                    for ref_ in ref
-                ]
-            )
-            # scores = np.array(
-            #     [Rouge().get_scores(ref_, pred, avg=True)["rouge-l"]["f"] for ref_ in ref]
-            # )
-            rouge_l = np.mean(scores)
-
-            bleu_1_ = bleu_1 if bleu_1 > eps else 0
-            bleu_4_ = bleu_4 if bleu_4 > eps else 0
-            meteor_ = meteor if meteor > eps else 0
-            rouge_l_ = rouge_l if rouge_l > eps else 0
-
-        except ValueError:
+        if pred == "":
             bleu_1_, bleu_4_, meteor_, rouge_l_ = 0, 0, 0, 0
 
-        bleu_1 += bleu_1_
-        bleu_4 += bleu_4_
-        meteor += meteor_
-        rouge_l += rouge_l_
+        else:
+            try:
+                # Calculate BLEU score
+                ref_ = [x.split() for x in ref]
+                pred_ = pred.split()
 
+                bleu_1_ = sentence_bleu(ref_, pred_, weights=(1, 0, 0, 0))
+                bleu_4_ = sentence_bleu(ref_, pred_, weights=(0.25, 0.25, 0.25, 0.25))
+
+                # Calculate METEOR
+                meteor_ = meteor_score(ref, pred)
+
+                # Calculate ROUGE-L
+                scores = np.array(
+                    [
+                        Rouge().get_scores(ref_, pred, avg=True)["rouge-l"]["f"]
+                        if ref != ""
+                        else 0
+                        for ref_ in ref
+                    ]
+                )
+                rouge_l_ = np.mean(scores)
+            except ValueError:
+                bleu_1_, bleu_4_, meteor_, rouge_l_ = 0, 0, 0, 0
+
+        bleu_1 += bleu_1_ if bleu_1_ > eps else 0
+        bleu_4 += bleu_4_ if bleu_4_ > eps else 0
+        meteor += meteor_ if meteor_ > eps else 0
+        rouge_l += rouge_l_ if rouge_l_ > eps else 0
         n_samples += 1
 
     return (