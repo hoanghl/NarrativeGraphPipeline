a48580cfc7b1eb9924ff630a7e1b576f3150f010
Superpod <v.hoanglh88@vinai.io>
2021-09-13 23:45:19 +0700

    Modify module tune and loss logging method

--- models/narrative_model.py
+++ models/narrative_model.py
@@ -33,11 +33,11 @@ class NarrativeModel(plt.LightningModule):
         max_epochs,
         warmup_rate,
         path_pretrained,
-        path_pred,
+        path_test_pred,
         path_train_pred,
         path_valid_pred,
-        num_gpus,
-        tuning=False,
+        use_2_answers,
+        is_tuning=False,
     ):
 
         super().__init__()
@@ -50,11 +50,11 @@ class NarrativeModel(plt.LightningModule):
         self.warmup_rate = warmup_rate
         self.size_dataset_train = size_dataset_train
         self.max_epochs = max_epochs
-        self.path_pred = path_pred
+        self.path_test_pred = path_test_pred
         self.path_train_pred = path_train_pred
         self.path_valid_pred = path_valid_pred
-        self.num_gpus = num_gpus
-        self.tuning = tuning
+        self.use_2_answers = use_2_answers
+        self.is_tuning = is_tuning
         self.bert_tokenizer = BertTokenizer.from_pretrained(path_pretrained)
 
         os.makedirs(os.path.dirname(path_valid_pred), exist_ok=True)
@@ -88,177 +88,6 @@ class NarrativeModel(plt.LightningModule):
         # for params in list_freeze_sets:
         #     for param in params:
         #         param.requires_grad = False
-
-    ####################################################################
-    # FOR TRAINING PURPOSE
-    ####################################################################
-    def get_prediction(self, pairs):
-        pairs = [
-            {
-                "pred": " ".join(self.bert_tokenizer.convert_ids_to_tokens(pred)),
-                "trg": " ".join(self.bert_tokenizer.convert_ids_to_tokens(trg)),
-            }
-            for pred, trg in zip(pairs["pred"], pairs["trg"])
-        ]
-
-        return pairs
-
-    def training_step(self, batch: Any, batch_idx: int):
-        loss, logist = self.model.do_train(
-            q=batch["q_ids"],
-            c=batch["c_ids"],
-            a1=batch["a1_ids"],
-            a2=batch["a2_ids"],
-            a1_mask=batch["a1_masks"],
-            a2_mask=batch["a2_masks"],
-            use_2_ans=True,
-        )
-        # logist: list of [b, la, d_vocab]
-        self.log("train/loss_step", loss)
-
-        logist = [torch.argmax(logist_, dim=-1) for logist_ in logist]
-        trgs_ = [batch["a1_ids"], batch["a2_ids"]] if len(logist) > 1 else [batch["a1_ids"]]
-
-        bz = batch["q_ids"].size(0)
-        preds, trgs = [], []
-        for i in range(bz):
-            for output, trg in zip(logist, trgs_):
-                preds.append(output[i])
-                trgs.append(trg[i])
-
-        return {
-            "loss": loss,
-            "pred": preds,
-            "trg": trgs,
-            "size_pred": logist[0].size(-1),
-            "size_trg": trgs_[0].size(-1),
-        }
-
-    def training_step_end(self, batch_parts):
-        size_pred = batch_parts["size_pred"][0]
-        size_trg = batch_parts["size_trg"][0]
-
-        preds, trgs = [], []
-        for pred, trg in zip(batch_parts["pred"], batch_parts["trg"]):
-            preds.extend(pred.view(-1, size_pred).cpu().detach())
-            trgs.extend(trg.view(-1, size_trg).cpu().detach())
-
-        return {"loss": batch_parts["loss"].mean(), "pred": preds, "trg": trgs}
-
-    def training_epoch_end(self, outputs) -> None:
-        outputs = outputs[0]
-
-        ## Calculate mean loss
-        self.log("train/loss_epoch", outputs["loss"])
-
-        ## Calculate B-1, B-4, METEOR and ROUGE-L
-        outputs = self.get_prediction(outputs)
-
-        with open(self.path_train_pred, "a+") as pred_file:
-            json.dump(outputs, pred_file, indent=2, ensure_ascii=False)
-
-        bleu_1, bleu_4, meteor, rouge_l = get_scores(outputs)
-
-        self.log("train/bleu_1", bleu_1)
-        self.log("train/bleu_4", bleu_4)
-        self.log("train/meteor", meteor)
-        self.log("train/rouge_l", rouge_l)
-
-    def test_step(self, batch: Any, batch_idx):
-        return 0
-
-    def validation_step(self, batch: Any, batch_idx):
-        if self.tuning is True:
-            return 0
-
-        logist = self.model.do_predict(batch["q_ids"], batch["c_ids"], self.la)
-        # logist: [b, la]
-
-        logist = [logist, logist]
-        trgs_ = [batch["a1_ids"], batch["a2_ids"]]
-
-        bz = batch["q_ids"].size(0)
-        preds, trgs = [], []
-        for i in range(bz):
-            for output, trg in zip(logist, trgs_):
-                preds.append(output[i])
-                trgs.append(trg[i])
-
-        return {
-            "pred": preds,
-            "trg": trgs,
-            "size_pred": logist[0].size(-1),
-            "size_trg": trgs_[0].size(-1),
-        }
-
-    def validation_step_end(self, batch_parts):
-        size_pred = batch_parts["size_pred"][0]
-        size_trg = batch_parts["size_trg"][0]
-
-        preds, trgs = [], []
-        for pred, trg in zip(batch_parts["pred"], batch_parts["trg"]):
-            preds.extend(pred.view(-1, size_pred).cpu().detach())
-            trgs.extend(trg.view(-1, size_trg).cpu().detach())
-
-        return {"pred": preds, "trg": trgs}
-
-    def validation_epoch_end(self, outputs) -> None:
-        if self.tuning is True:
-            return None
-
-        outputs = outputs[0]
-
-        outputs = self.get_prediction(outputs)
-
-        with open(self.path_valid_pred, "a+") as pred_file:
-            json.dump(outputs, pred_file, indent=2, ensure_ascii=False)
-
-        bleu_1, bleu_4, meteor, rouge_l = get_scores(outputs)
-
-        # if self.trainer.is_global_zero:
-        self.log("valid/bleu_1", bleu_1)
-        self.log("valid/bleu_4", bleu_4)
-        self.log("valid/meteor", meteor)
-        self.log("valid/rouge_l", rouge_l)
-
-    def predict_step(self, batch, batch_idx: int, dataloader_idx: int = None):
-        if self.tuning is True:
-            return 0
-
-        logist = self.model.do_predict(batch["q_ids"], batch["c_ids"], self.la)
-        # logist: [b, la]
-
-        logist = [logist, logist]
-        trgs_ = [batch["a1_ids"], batch["a2_ids"]]
-
-        bz = batch["q_ids"].size(0)
-        preds, trgs = [], []
-        for i in range(bz):
-            for output, trg in zip(logist, trgs_):
-                preds.append(output[i])
-                trgs.append(trg[i])
-
-        return {"pred": preds, "trg": trgs}
-
-    def predict_step_end(self, batch_parts):
-        preds, trgs = [], []
-        for pred, trg in zip(batch_parts["pred"], batch_parts["trg"]):
-            preds.extend(pred.view(self.num_gpus, -1))
-            trgs.extend(trg.view(self.num_gpus, -1))
-
-        return {"pred": preds, "trg": trgs}
-
-    def on_predict_epoch_end(self, results):
-        outputs = results[0]
-
-        outputs = self.get_prediction(outputs)
-
-        with open(self.path_pred, "a+") as pred_file:
-            json.dump(outputs, pred_file, indent=2, ensure_ascii=False)
-
-        bleu_1, bleu_4, meteor, rouge_l = get_scores(outputs)
-
-        return bleu_1, bleu_4, meteor, rouge_l
 
     def configure_optimizers(self):
         no_decay = ["bias", "LayerNorm.weight"]
@@ -294,3 +123,222 @@ class NarrativeModel(plt.LightningModule):
             "frequency": 1,
         }
         return [optimizer], [lr_scheduler]
+
+    ####################################################################
+    # FOR TRAINING PURPOSE
+    ####################################################################
+
+    def get_prediction(self, pairs):
+        pairs = [
+            {
+                "pred": " ".join(self.bert_tokenizer.convert_ids_to_tokens(pred)),
+                "trg": " ".join(self.bert_tokenizer.convert_ids_to_tokens(trg)),
+            }
+            for pred, trg in zip(pairs["pred"], pairs["trg"])
+        ]
+
+        return pairs
+
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, logist = self.model.do_train(
+            q=batch["q_ids"],
+            c=batch["c_ids"],
+            a1=batch["a1_ids"],
+            a2=batch["a2_ids"],
+            a1_mask=batch["a1_masks"],
+            a2_mask=batch["a2_masks"],
+            use_2_ans=self.use_2_answers,
+        )
+        # logist: list of [b, la, d_vocab]
+
+        if not self.is_tuning:
+            logist = [torch.argmax(logist_, dim=-1) for logist_ in logist]
+            trgs_ = [batch["a1_ids"], batch["a2_ids"]] if self.use_2_answers else [batch["a1_ids"]]
+
+            bz = batch["q_ids"].size(0)
+            preds, trgs = [], []
+            for i in range(bz):
+                for output, trg in zip(logist, trgs_):
+                    preds.append(output[i])
+                    trgs.append(trg[i])
+
+            return {
+                "loss": loss,
+                "pred": preds,
+                "trg": trgs,
+                "size_pred": logist[0].size(-1),
+                "size_trg": trgs_[0].size(-1),
+            }
+        return {"loss": loss}
+
+    def training_step_end(self, batch_parts):
+        if self.is_tuning is True:
+            return {"loss": batch_parts["loss"].mean()}
+
+        size_pred = (
+            batch_parts["size_pred"]
+            if isinstance(batch_parts["size_pred"], int)
+            else batch_parts["size_pred"][0]
+        )
+        size_trg = (
+            batch_parts["size_trg"]
+            if isinstance(batch_parts["size_trg"], int)
+            else batch_parts["size_trg"][0]
+        )
+
+        preds, trgs = [], []
+        for pred, trg in zip(batch_parts["pred"], batch_parts["trg"]):
+            preds.extend(pred.view(-1, size_pred).cpu().detach())
+            trgs.extend(trg.view(-1, size_trg).cpu().detach())
+
+        loss = batch_parts["loss"].mean()
+        self.log("train/loss", loss, on_step=True, on_epoch=True)
+
+        return {"loss": loss, "pred": preds, "trg": trgs}
+
+    def training_epoch_end(self, outputs) -> None:
+        if self.is_tuning is True:
+            return
+
+        pairs = {"pred": [], "trg": []}
+        loss = 0
+        for output in outputs:
+            pairs["pred"].extend(output["pred"])
+            pairs["trg"].extend(output["trg"])
+            loss += output["loss"]
+        outputs = pairs
+
+        ## Calculate B-1, B-4, METEOR and ROUGE-L
+        outputs = self.get_prediction(outputs)
+
+        with open(self.path_train_pred, "a+") as pred_file:
+            json.dump(outputs, pred_file, indent=2, ensure_ascii=False)
+
+        bleu_1, bleu_4, meteor, rouge_l = get_scores(outputs)
+
+        self.log("train/bleu_1", bleu_1)
+        self.log("train/bleu_4", bleu_4)
+        self.log("train/meteor", meteor)
+        self.log("train/rouge_l", rouge_l)
+
+    def test_step(self, batch: Any, batch_idx):
+        return 0
+
+    def validation_step(self, batch: Any, batch_idx):
+        logist = self.model.do_predict(batch["q_ids"], batch["c_ids"], self.la)
+        # logist: [b, la]
+
+        logist = [logist, logist] if self.use_2_answers else [logist]
+        trgs_ = [batch["a1_ids"], batch["a2_ids"]] if self.use_2_answers else [batch["a1_ids"]]
+
+        bz = batch["q_ids"].size(0)
+        preds, trgs = [], []
+        for i in range(bz):
+            for output, trg in zip(logist, trgs_):
+                preds.append(output[i])
+                trgs.append(trg[i])
+
+        return {
+            "pred": preds,
+            "trg": trgs,
+            "size_pred": logist[0].size(-1),
+            "size_trg": trgs_[0].size(-1),
+        }
+
+    def validation_step_end(self, batch_parts):
+        size_pred = (
+            batch_parts["size_pred"]
+            if isinstance(batch_parts["size_pred"], int)
+            else batch_parts["size_pred"][0]
+        )
+        size_trg = (
+            batch_parts["size_trg"]
+            if isinstance(batch_parts["size_trg"], int)
+            else batch_parts["size_trg"][0]
+        )
+
+        preds, trgs = [], []
+        for pred, trg in zip(batch_parts["pred"], batch_parts["trg"]):
+            preds.extend(pred.view(-1, size_pred).cpu().detach())
+            trgs.extend(trg.view(-1, size_trg).cpu().detach())
+
+        return {"pred": preds, "trg": trgs}
+
+    def validation_epoch_end(self, outputs) -> None:
+        pairs = {"pred": [], "trg": []}
+        for output in outputs:
+            pairs["pred"].extend(output["pred"])
+            pairs["trg"].extend(output["trg"])
+        outputs = pairs
+
+        outputs = self.get_prediction(outputs)
+
+        with open(self.path_valid_pred, "a+") as pred_file:
+            json.dump(outputs, pred_file, indent=2, ensure_ascii=False)
+
+        bleu_1, bleu_4, meteor, rouge_l = get_scores(outputs)
+
+        self.log("valid/bleu_1", bleu_1)
+        self.log("valid/bleu_4", bleu_4)
+        self.log("valid/meteor", meteor)
+        self.log("valid/rouge_l", rouge_l)
+
+    def test_step(self, batch, batch_idx):
+        logist = self.model.do_predict(batch["q_ids"], batch["c_ids"], self.la)
+        # logist: [b, la]
+
+        logist = [logist, logist] if self.use_2_answers else [logist]
+        trgs_ = [batch["a1_ids"], batch["a2_ids"]] if self.use_2_answers else [batch["a1_ids"]]
+
+        bz = batch["q_ids"].size(0)
+        preds, trgs = [], []
+        for i in range(bz):
+            for output, trg in zip(logist, trgs_):
+                preds.append(output[i])
+                trgs.append(trg[i])
+
+        return {
+            "pred": preds,
+            "trg": trgs,
+            "size_pred": logist[0].size(-1),
+            "size_trg": trgs_[0].size(-1),
+        }
+
+    def test_step_end(self, batch_parts):
+        size_pred = (
+            batch_parts["size_pred"]
+            if isinstance(batch_parts["size_pred"], int)
+            else batch_parts["size_pred"][0]
+        )
+        size_trg = (
+            batch_parts["size_trg"]
+            if isinstance(batch_parts["size_trg"], int)
+            else batch_parts["size_trg"][0]
+        )
+
+        preds, trgs = [], []
+        for pred, trg in zip(batch_parts["pred"], batch_parts["trg"]):
+            preds.extend(pred.view(-1, size_pred).cpu().detach())
+            trgs.extend(trg.view(-1, size_trg).cpu().detach())
+
+        return {"pred": preds, "trg": trgs}
+
+    def test_epoch_end(self, outputs) -> None:
+        pairs = {"pred": [], "trg": []}
+        for output in outputs:
+            pairs["pred"].extend(output["pred"])
+            pairs["trg"].extend(output["trg"])
+        outputs = pairs
+
+        outputs = self.get_prediction(outputs)
+
+        if not self.is_tuning:
+            with open(self.path_test_pred, "a+") as pred_file:
+                json.dump(outputs, pred_file, indent=2, ensure_ascii=False)
+
+        bleu_1, bleu_4, meteor, rouge_l = get_scores(outputs)
+
+        self.log("bleu_1", bleu_1)
+        self.log("bleu_4", bleu_4)
+        self.log("meteor", meteor)
+        self.log("rouge_l", rouge_l)
--- tune.py
+++ tune.py
@@ -27,10 +27,10 @@ def main(config: DictConfig):
     if config.multigpu is True:
         config.trainer.precision = 32
         config.trainer.gpus = -1
-        config.trainer.accelerator = "ddp"
+        config.trainer.accelerator = "dp"
         config.trainer.replace_sampler_ddp = True
 
-    config.trainer.max_epochs = 10
+    config.trainer.check_val_every_n_epoch = 100
 
     utils.extras(config)
     utils.print_config(config, resolve=True)
@@ -38,7 +38,7 @@ def main(config: DictConfig):
     log.info("########### Start tuning! ###########")
 
     # Init lightning loggers
-    default_log = "tensorboard" if "log" not in config else config.log
+    default_log = "wandb" if "log" not in config else config.log
     logger: List[LightningLoggerBase] = []
     if "logger" in config and config.logger is not None:
         for name, lg_conf in config.logger.items():
@@ -67,7 +67,7 @@ def main(config: DictConfig):
 
     # Init lightning model
     log.info(f"Instantiating model <{config.model._target_}>")
-    model: LightningModule = hydra.utils.instantiate(config.model, num_gpus=trainer.num_gpus)
+    model: LightningModule = hydra.utils.instantiate(config.model, is_tuning=True)
 
     # Send some parameters from config to all lightning loggers
     log.info("Logging hyperparameters!")
@@ -85,7 +85,13 @@ def main(config: DictConfig):
     trainer.fit(model=model, datamodule=datamodule)
 
     log.info("Starting predicting!")
-    bleu_1, bleu_4, meteor, rouge_l = trainer.predict(model=model, datamodule=datamodule)
+    output = trainer.test(model=model, datamodule=datamodule)
+    bleu_1, bleu_4, meteor, rouge_l = (
+        output[0]["bleu_1"],
+        output[0]["bleu_4"],
+        output[0]["meteor"],
+        output[0]["rouge_l"],
+    )
 
     # Make sure everything closed properly
     log.info("Finalizing!")
@@ -95,6 +101,7 @@ def main(config: DictConfig):
         datamodule=datamodule,
         trainer=trainer,
         logger=logger,
+        callbacks=None,
     )
 
     return bleu_1
